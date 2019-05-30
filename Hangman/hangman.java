2fd82eff8db6a98d4b8dbecad433aabce4311026
Rasmus69 <Rasmus0526@gmail.com>
2019-05-30 17:30:03 +0200

    Rätt 3
    fixade det sista som inte funkade samt gjorde min kod lite "fräshare"

--- Hangman/hangman.java
+++ Hangman/hangman.java
@@ -4,163 +4,176 @@ import java.util.Random;
 import hangman.HangmanConsoleWindow;
 
 public class hangman {
+
+	static HangmanConsoleWindow hcw = new HangmanConsoleWindow();
+	static String hemligtord;
+	static String rättgissade = "";
+	static String felgissade = "";
+	static int liv;
+	//static String[] Ord = {"hej"};
+	//static String[] Ordhard = {"hej"};
+	static String[] Ord = {"Ord", "Klok", "NTI", "Skola", "Idrott", "kanske", "Tycker"}; // <--Ordlistan för lätt
+	static String[] Ordhard = {"Impertinent", "Hegemoni", "Verserad", "Kompilera", "Cinematek", "Renegat", "Karessera"}; // <--Ordlistan för svår
+
+	static String[] gubbe = {
+			"  +---+\r\n" + 
+			"  |   |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"=========",
+			"  +---+\r\n" + 
+			"  |   |\r\n" + 
+			"  O   |\r\n" + 
+			"  |   |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"=========",
+			"  +---+\r\n" + 
+			"  |   |\r\n" + 
+			"  O   |\r\n" + 
+			" /|   |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"=========",
+			"  +---+\r\n" + 
+			"  |   |\r\n" + 
+			"  O   |\r\n" + 
+			" /|\\  |\r\n" + 
+			"      |\r\n" + 
+			"      |\r\n" + 
+			"=========",
+			"  +---+\r\n" + 
+			"  |   |\r\n" + 
+			"  O   |\r\n" + 
+			" /|\\  |\r\n" + 
+			" / \\  |\r\n" + 
+			"      |\r\n" + 
+			"========="	
+	};
 	
-		static HangmanConsoleWindow hcw = new HangmanConsoleWindow();
-		static String hemligtord;
-		static String rättgissade = "";
-		static String felgissade = "";
-		static int liv = 0;
-		static String[] Ord = {"hej"};
-		static String[] Ordhard = {"hej"};
-		//static String[] Ord = {"Ord", "Rätt", "NTI", "Skola", "Idrott", "kanske", "Tycker"}; // <--Ordlistan för lätt
-		//static String[] Ordhard = {"Impertinent", "Hegemoni", "Verserad", "Kompilera", "Cinematek", "Renegat", "Karessera"}; // <--Ordlistan för svår
-		
-		static String[] gubbe = {
-				"  +---+\r\n" + 
-				"  |   |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"=========",
-				"  +---+\r\n" + 
-				"  |   |\r\n" + 
-				"  O   |\r\n" + 
-				"  |   |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"=========",
-				"  +---+\r\n" + 
-				"  |   |\r\n" + 
-				"  O   |\r\n" + 
-				" /|   |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"=========",
-				"  +---+\r\n" + 
-				"  |   |\r\n" + 
-				"  O   |\r\n" + 
-				" /|\\  |\r\n" + 
-				"      |\r\n" + 
-				"      |\r\n" + 
-				"=========",
-				"  +---+\r\n" + 
-				"  |   |\r\n" + 
-				"  O   |\r\n" + 
-				" /|\\  |\r\n" + 
-				" / \\  |\r\n" + 
-				"      |\r\n" + 
-				"========="	
-		};
-		
+
+	public static void main(String[] args) {
+		start();
+	}
 	
-		public static void main(String[] args){
-			start();
+	public static void start() {
+		hcw.clear();
+		hcw.println("Välkommen till mitt spel.");
+		hcw.println("");
+		hcw.println("Klicka på 1 om du vill starta spelet.");
+		hcw.println("");
+		hcw.println("Klicka på 2 om du vill starta spelet");
+		hcw.println("med svårare ord.");
+		hcw.println("");
+		hcw.println("(Detta spelet har ej med bokstäverna");
+		hcw.println("'å' 'ä' 'ö').");
+		
+		//Här väljder den ett av orden som finns i arraylisten.
+		//Det är även en switch så man kan välja om man vill köra på lätt nivå eller svår nivå.
+		
+		char input = hcw.nextChar();
+		switch (input) {
+			case '1':
+				hemligtord = Ord[(new Random()).nextInt(Ord.length)];
+				startaspel();
+				break;
+			case '2':
+				hemligtord = Ordhard[(new Random()).nextInt(Ordhard.length)];
+				startaspel();
+			default:
+				start();
+				break;
+				
+	}
+	}
+	
+	public static void startaspel() {
+		for(int i = 0; i < hemligtord.length(); i++){
+			rättgissade += "*";
 		}
 		
-		public static void start() {
-			hcw.clear();
-			hcw.println("Välkomen till mit spel.");
-			hcw.println("");
-			hcw.println("Klicka på 1 om du önskas börja spelet.");
-			hcw.println("");
-			hcw.println("Klicka på 2 om du önskas börja spelet");
-			hcw.println("med svårare ord.");
-			hcw.println("");
-			hcw.println("(Detta spelet har ej ord med bokstäverna");
-			hcw.println("'å' 'ä' 'ö').");
-			
-			//Här väljder den ett av orden som finns i arraylisten.
-			//Det är även en switch så man kan välja om man vill köra på lätt nivå eller svår nivå.
-			
-			char input = hcw.nextChar();
-			switch (input) {
-				case '1':
-					hemligtord = Ord[(new Random()).nextInt(Ord.length)];
-					startaspel();
-					break;
-				case '2':
-					hemligtord = Ordhard[(new Random()).nextInt(Ordhard.length)];
-					startaspel();
-				default:
-					start();
-					break;
-					
-			}
-		}
+		skrivut();
+	}
+	
+	public static void skrivut() {
+		kollaliv();
+		hcw.clear();
+		hcw.println("Detta är din gubbe.");
+		hcw.println(gubbe[liv]);
+		hcw.println(rättgissade);
+		hcw.println("Gissa bokstav här.");
 		
-		public static void startaspel() {
-			for(int i = 0; i < hemligtord.length(); i++){
-				rättgissade += "*";
-			}
-			
-			skrivut();
-			
-		}
+		char bokstav = hcw.nextChar();
 		
-		public static void skrivut() {
-			kollaliv();
-			hcw.clear();
-			hcw.println("Detta är din gubbe.");
-			hcw.println(gubbe[liv]);
-			hcw.println(rättgissade);
-			hcw.println("Gissa bokstav här.");
-			
-			char bokstav = hcw.nextChar();
-			
-			boolean rättbokstav = false;
-			int i = 0;
-			for(i = 0; i < hemligtord.length(); i++) {
-				if( hemligtord.charAt(i) == bokstav) {
+		boolean rättbokstav = false;
+		int i = 0;
+		for(i = 0; i < hemligtord.length(); i++) {
+			if( hemligtord.charAt(i) == bokstav) {
 					rättbokstav = true;
-					break;
-				}
+				break;
 			}
-			if(rättbokstav == true) {
-				char[] rättgissadearray = rättgissade.toCharArray();
-				rättgissadearray[i] = bokstav;
-				rättgissade = String.valueOf(rättgissadearray);
-			}else {
-				felgissade += bokstav;
-				liv += 1; 
-			}
-			
-			//Kolla om man förlorat
-			
-			if(rättgissade.indexOf('*') < 0) {
-				hcw.clear();
-				hcw.println("Grattis du van spelet.");
-				hcw.println("Vill du spela igen?");
-				hcw.println("Klicka på 1 för att starta om.");
-				
-				char val = hcw.nextChar();
-				switch (val) {
-				case '1':
-					start();
-					break;
-				default:
-					break;
-				}
-			}
-			
-			skrivut();
+		}
+		if(rättbokstav == true) {
+			char[] rättgissadearray = rättgissade.toCharArray();
+			rättgissadearray[i] = bokstav;
+			rättgissade = String.valueOf(rättgissadearray);
+		}else {
+			felgissade += bokstav;
+			liv += 1;
+			System.out.print(liv);
 		}
 		
-		public static void kollaliv() {
-			if(liv == gubbe.length ) {
-				hcw.clear();
-				hcw.println("Du har förlorat.");
-				hcw.println("Vill du spela igen?");
-				hcw.println("Klicka på 1 för att starta om.");
-				
-				char val = hcw.nextChar();
-				switch (val) {
+		//Går in ifall man vinner
+		
+		if(rättgissade.indexOf('*') < 0) {
+			hcw.clear();
+			hcw.println("Grattis du vann spelet.");
+			hcw.println("Vill du spela igen?");
+			hcw.println("Klicka på 1 för att starta om.");
+			hcw.println();
+			hcw.println("Klicka på vad som helst förutom 1");
+			hcw.println("för att avsluta spelet.");
+			
+			char val = hcw.nextChar();
+			switch (val) {
 				case '1':
+					rättgissade = "";
 					start();
 					break;
 				default:
+					System.exit(0);
 					break;
-				}
 			}
 		}
+		
+		skrivut();
+	}
+	
+	//Går in ifall man förlorar
+
+	public static void kollaliv() {
+		if(liv == gubbe.length ) {
+			hcw.clear();
+			hcw.println("Du har förlorat.");
+			hcw.println("Vill du spela igen?");
+			hcw.println("Klicka på 1 för att starta om.");
+			hcw.println();
+			hcw.println("Klicka på vad som helst förutom 1");
+			hcw.println("för att avsluta spelet.");
+			
+			char val = hcw.nextChar();
+			switch (val) {
+				case '1':
+					liv = 0;
+					rättgissade = "";
+					start();
+					break;
+				default:
+					System.exit(0);
+					break;
+			}
+		}
+	}
 }
